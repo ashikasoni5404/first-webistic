2a098cb9e0fd98c2ff11eabc965f54e66bc11e1d
ashikasoni5404 <ashikasoni5858@gmail.com>
2024-02-26 22:23:20 +0530

    slider added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import './App.css';
 import Contact from './components/contact/Contact';
 import Card from './components/card/Card';
 import Template from './components/template/Template';
+import Slider from './components/slider/Slider';
 function App() {
   return (
     <div className="App">
-      <Contact/>
-      <Card/>
-      <Template/>
+      <Slider/>
+
       
     </div>
   );
