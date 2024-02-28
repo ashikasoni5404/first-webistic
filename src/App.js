0926855f4d09f15e791662f2bf9e0200b4a42c2f
ashikasoni5404 <ashikasoni5858@gmail.com>
2024-02-28 14:03:38 +0530

    custom section

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ import Template from './components/template/Template';
 import Slider from './components/slider/Slider';
 import Counter from './components/counter/Counter';
 import Footer from './components/footer/Footer';
+import Custom from './components/custom/Custom';
 function App() {
   return (
     <div className="App">
-      <Footer/>
+    <Custom/>
 
       
     </div>
---
+++ src/components/custom/Custom.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./custom.css"
+const Custom = () => {
+    return (
+        <div>
+            <div className="container main-custom">
+                <div className="c-head"><p>CUSTOM SYNTHESIS & ANALYSIS</p></div>
+                <div>
+                    <div class="line-container">
+                        <div class="line"><span>O</span></div>
+                    </div>
+                </div>
+                <div className="c-content">
+                    <h3 className="c-para">
+                        Our Synthesis Service team chemists are skilled in performing complex, multi-step synthesis and solving challenging chemistry problems. Our Analytical Services team consists of highly skilled scientists with expertise in modern analytical techniques, supported by state-of-the-art instruments.
+                    </h3>
+                </div>
+                <div className="img-sec">
+                <img  className="c-img" src="https://artisbiotech.com/images/laptops1.png" class="img-fluid" alt="https://artisbiotech.com/images/laptops1.png"/>
+                </div>
+            </div>
+
+           
+          
+           
+          
+        </div>
+    )
+}
+export default Custom;
